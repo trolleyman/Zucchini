409f4d973fcfaaa02899dd062dfa1ee49cba5544
Callum Tolley <cgtrolley@gmail.com>
2017-01-19 18:17:48 +0000

    Added structure of Image class

---
+++ src/game/render/Image.java
@@ -0,0 +1,14 @@
+package game.render;
+
+import java.nio.ByteBuffer;
+
+public class Image {
+	public ByteBuffer data;
+	public int w;
+	public int h;
+	public int texID;
+	
+	public Image(String path) {
+		// Load image at path
+	}
+}
