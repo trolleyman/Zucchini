7d73ea5ac81c338f3acd2c175682508c3edbff4d
Jack Morrison <jack.morrison6600@gmail.com>
2017-01-27 14:45:15 +0000

    Added exit button to StartUI

--- src/game/ui/StartUI.java
+++ src/game/ui/StartUI.java
@@ -14,7 +14,8 @@ import game.render.TextureBank;
  */
 public class StartUI extends UI {
 	
-	private UIButton button;
+	private UIButton startButton;
+	private UIButton exitButton;
 	private UI nextUI = this;
 	float windowW;
 	float windowH;
@@ -25,28 +26,44 @@ public class StartUI extends UI {
 		windowW = renderer.getWidth();
 		windowH = renderer.getHeight();
 		
-		button = new UIButton(
+		startButton = new UIButton(
 			() -> { this.nextUI = new GameUI(renderer, null); },
 			100, 100,
 			renderer.getImageBank().getTexture("buttonDefault.png"),
 			renderer.getImageBank().getTexture("buttonHover.png"),
 			renderer.getImageBank().getTexture("buttonPressed.png")
 		);
-		this.inputHandlers.add(button);
+		
+		exitButton = new UIButton(
+				() -> { /* TODO Exit */ },
+				100, 100,
+				renderer.getImageBank().getTexture("exitButtonDefault.png"),
+				renderer.getImageBank().getTexture("exitButtonHover.png"),
+				renderer.getImageBank().getTexture("exitButtonPressed.png")
+			);
+		
+		//renderer.drawImage(renderer.getImageBank().getTexture("startBackground.png"), 500, 400);
+		
+		this.inputHandlers.add(exitButton);
+		this.inputHandlers.add(startButton);
 	}
 	
 	@Override
 	public void update(double dt) {
 		windowW = renderer.getWidth();
 		windowH = renderer.getHeight();
-		button.update(dt);
+		startButton.update(dt);
+		startButton.update(dt);
 	}
 	
 	@Override
 	public void render(IRenderer r) {
-		button.setX((int) (windowW/2.0 - button.getWidth()/2.0));
-		button.setY((int) (windowH/2.0 - button.getHeight()/2.0));
-		button.render(r);
+		startButton.setX((int) (windowW/2.0 - startButton.getWidth()/2.0));
+		startButton.setY((int) (windowH/2.0 - startButton.getHeight()/2.0));
+		exitButton.setX((int) (windowW - (exitButton.getWidth()) - 20.0));
+		exitButton.setY((int) (windowH - (exitButton.getHeight()) - 20.0));
+		startButton.render(r);
+		exitButton.render(r);
 	}
 	
 	@Override
