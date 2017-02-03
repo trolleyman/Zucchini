268008753c42bb4e404eb181f44aaa69c3478e1c
vxz522 <vxz522@cs.bham.ac.uk>
2017-02-03 13:53:19 +0000

    minor fix&add 2

--- src/game/networking/server/threads/udp/UDPListenerLobbyThread.java
+++ src/game/networking/server/threads/udp/UDPListenerLobbyThread.java
@@ -1,19 +1,34 @@
 package game.networking.server.threads.udp;
 
+import java.net.DatagramSocket;
+
 //FAST: implement this!!!
 
 public class UDPListenerLobbyThread implements Runnable
 {
+	private DatagramSocket socket;
+	private boolean run = false;
 
-	public UDPListenerLobbyThread()
+	public UDPListenerLobbyThread(DatagramSocket _socket)
 	{
-
+		socket = _socket;
 	}
 
 	@Override
 	public void run()
 	{
 
+	}
+
+	public synchronized void StartGame()
+	{
+		run = true;
+		(new Thread(this)).start();
+	}
+
+	public synchronized void StopGame()
+	{
+		run = false;
 	}
 
 }
