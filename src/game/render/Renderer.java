6b874af9cc02ffd98d5c060428a7550a664a11a1
Callum Tolley <cgtrolley@gmail.com>
2017-02-05 15:33:14 +0000

    Added comments

--- src/game/render/Renderer.java
+++ src/game/render/Renderer.java
@@ -186,10 +186,11 @@ public class Renderer implements IRenderer {
 		// Enable v-sync
 		this.setVSync(true);
 		
+		// Set OpenGL settings
 		glEnable(GL_BLEND);
 		glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 		
-		glDisable(GL_CULL_FACE);
+		glEnable(GL_CULL_FACE);
 		
 		// Set the clear color
 		glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
