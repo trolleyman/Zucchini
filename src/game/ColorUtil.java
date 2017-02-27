5ad1eb626fc85967f63a49567e9bf9ddc4ac2597
Jack Morrison <jack.morrison6600@gmail.com>
2017-02-27 18:17:53 +0000

    Improved lobby visuals

--- src/game/ColorUtil.java
+++ src/game/ColorUtil.java
@@ -13,6 +13,7 @@ import org.joml.Vector4f;
 public class ColorUtil {
 	public static final Vector4f WHITE = new Vector4f(1.0f, 1.0f, 1.0f, 1.0f);
 	public static final Vector4f BLACK = new Vector4f(0.0f, 0.0f, 0.0f, 1.0f);
+	public static final Vector4f TRANSPARENT = new Vector4f(0.0f, 0.0f, 0.0f, 0.0f);
 	
 	public static final Vector4f RED   = new Vector4f(1.0f, 0.0f, 0.0f, 1.0f);
 	public static final Vector4f GREEN = new Vector4f(0.0f, 1.0f, 0.0f, 1.0f);
@@ -21,4 +22,8 @@ public class ColorUtil {
 	public static final Vector4f YELLOW = new Vector4f(1.0f, 1.0f, 0.0f, 1.0f);
 	public static final Vector4f PINK   = new Vector4f(1.0f, 0.0f, 1.0f, 1.0f);
 	public static final Vector4f CYAN   = new Vector4f(0.0f, 1.0f, 1.0f, 1.0f);
+
+
+	public static final Vector4f LIGHT_GREY = new Vector4f(0.8f, 0.8f, 0.8f, 1.0f);
+	public static final Vector4f DARK_GREY = new Vector4f(0.2f, 0.2f, 0.2f, 1.0f);
 }
--- src/game/ui/TextButtonComponent.java
+++ src/game/ui/TextButtonComponent.java
@@ -11,37 +11,38 @@ import static org.lwjgl.glfw.GLFW.*;
 public class TextButtonComponent extends UIComponent {
 
 	/** The function that is called when the button is clicked */
-	//TODO: Is this needed?
 	private Runnable callback;
-	
+
 	private boolean selected = false;
-	
+
 	private String s;
-	
+
 	private Font f;
 	private Align a;
 	private float scale;
-	
-	private Vector4f colour = ColorUtil.BLUE;
-	
+
+	private Vector4f box_colour = ColorUtil.BLACK;
+    private Vector4f border_colour = ColorUtil.WHITE;
+
 	/** The current mouse x */
 	private float mx;
 	/** The current mouse y */
 	private float my;
-	
+
 	/** The current button x */
 	private float x;
 	/** The current button y */
 	private float y;
-	
+
 	private float BOX_W;
 	private float BOX_H;
-	
+	private float border_width;
+
 	/** Whether the mouse has been pressed down */
 	private boolean pressed = false;
 	/** Whether the mouse has been released since the last update */
 	private boolean released = false;
-	
+
 	public TextButtonComponent(Runnable _callback, Align _a, float _x, float _y, Font _f, String _s, float scale) {
 		this.callback = _callback;
 		this.a = _a;
@@ -50,10 +51,12 @@ public class TextButtonComponent extends UIComponent {
 		this.f = _f;
 		this.s = _s;
 		this.scale = scale;
-		
-		BOX_W = 500;
-		BOX_H = 64*scale;
-		
+
+
+		border_width = 5;
+		BOX_W = 510;
+		BOX_H = 64*scale+10;
+
 	}
 
 	@Override
@@ -61,23 +64,23 @@ public class TextButtonComponent extends UIComponent {
 		this.mx = (float) xpos;
 		this.my = (float) ypos;
 	}
-	
+
 	@Override
 	public void handleMouseButton(int button, int action, int mods) {
 		if (action == GLFW_PRESS && button == GLFW_MOUSE_BUTTON_1) {
 			this.pressed = true;
 		}
-		
+
 		if (action == GLFW_RELEASE && button == GLFW_MOUSE_BUTTON_1) {
 			this.released = true;
 		}
 	}
-	
+
 	private boolean isMouseOnButton() {
 		return mx >= x && mx < x + BOX_W
 			&& my >= y && my < y + BOX_H;
 	}
-	
+
 	@Override
 	public void update(double dt) {
 		if (this.released) {
@@ -86,38 +89,39 @@ public class TextButtonComponent extends UIComponent {
 			if (isMouseOnButton())
 				this.callback.run();
 		}
-		
+
 		if (selected) {
-			colour = ColorUtil.GREEN;
+            border_colour = ColorUtil.WHITE;
 		} else if (isMouseOnButton()) {
-			colour = ColorUtil.BLUE;
+            border_colour = ColorUtil.LIGHT_GREY;
 		} else {
-			colour = ColorUtil.BLACK;
+            border_colour = ColorUtil.DARK_GREY;
 		}
 	}
 
 	@Override
 	public void render(IRenderer r) {
-		r.drawBox(a, x, y, BOX_W, BOX_H, colour);
-		r.drawText(f, s, Align.TL, x+10, y+14, scale);
+		r.drawBox(a, x, y, BOX_W, BOX_H, border_colour);
+		r.drawBox(a, x+border_width, y+border_width, BOX_W-2*border_width, BOX_H-2*border_width, box_colour);
+		r.drawText(f, s, Align.TL, x+15, y+19, scale);
 	}
-	
+
 	public boolean getSelected() {
 		return selected;
 	}
-	
+
 	public void setSelected(boolean b) {
 		selected = b;
 	}
-	
+
 	public String getString() {
 		return s;
 	}
-	
+
 	public void setString(String s) {
 		this.s = s;
 	}
-	
+
 	/**
 	 * Sets the y co-ordinate of the button
 	 * @param _x The x co-ordinate
@@ -125,7 +129,7 @@ public class TextButtonComponent extends UIComponent {
 	public void setX(float _x) {
 		this.x = _x;
 	}
-	
+
 	/**
 	 * Sets the y co-ordinate of the button
 	 * @param _y The y co-ordinate
@@ -133,14 +137,14 @@ public class TextButtonComponent extends UIComponent {
 	public void setY(float _y) {
 		this.y = _y;
 	}
-	
+
 	/**
 	 * Returns the width of the button
 	 */
 	public int getWidth() {
 		return (int) BOX_W;
 	}
-	
+
 	/**
 	 * Returns the height of the button
 	 */
