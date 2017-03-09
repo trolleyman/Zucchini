69f9d81e67860dd344db163ad204801c6d139759
vxz522 <vxz522@cs.bham.ac.uk>
2017-03-09 13:07:12 +0000

    added refresh button to refresh the lobbies

--- src/game/ui/LobbyUI.java
+++ src/game/ui/LobbyUI.java
@@ -14,62 +14,62 @@ import java.util.ArrayList;
 import static org.lwjgl.glfw.GLFW.GLFW_KEY_ESCAPE;
 import static org.lwjgl.glfw.GLFW.GLFW_PRESS;
 
-public class LobbyUI extends UI implements InputPipeMulti {
+public class LobbyUI extends UI implements InputPipeMulti
+{
 	/** The current window width */
 	private int windowW;
 	/** The current window height */
 	private int windowH;
-	
+
 	/** The list of objects to redirect input to */
 	private ArrayList<InputHandler> inputHandlers = new ArrayList<>();
-	
+
 	private float lobby_spacing = 80;
-	
+
 	/** The start button */
 	private ButtonComponent joinButton;
 	/** The exit button */
 	private ButtonComponent backButton;
+	/** The refresh lobby list button */
+	private TextButtonComponent refreshButton;
 	/** The background image */
 	private ImageComponent backgroundImage;
 	/** The next UI to return */
 	private UI nextUI = this;
-	
+
 	/** Test lobbies for button generation */
 	private ArrayList<LobbyInfo> lobbies = new ArrayList<>();
 	private LobbyInfo currentLobby = null;
-	
+
 	private ArrayList<TextButtonComponent> lobby_buttons = new ArrayList<>();
-	
-	public LobbyUI(UI _ui) {
+
+	public LobbyUI(UI _ui)
+	{
 		super(_ui);
-		
+
 		// Create Join Button
-		joinButton = new ButtonComponent(
-			() -> {
-				if (currentLobby != null)
-					this.nextUI = new LobbyWaitUI(this, currentLobby.getLobbyName());
-			},
-			Align.BL, 100, 100,
-			textureBank.getTexture("joinDefault.png"),
-			textureBank.getTexture("joinHover.png"),
-			textureBank.getTexture("joinPressed.png")
-		);
+		joinButton = new ButtonComponent(() -> {
+			if (currentLobby != null)
+				this.nextUI = new LobbyWaitUI(this, currentLobby.getLobbyName());
+		}, Align.BL, 100, 100, textureBank.getTexture("joinDefault.png"), textureBank.getTexture("joinHover.png"), textureBank.getTexture("joinPressed.png"));
 
 		// Create Back Button
-		backButton = new ButtonComponent(
-			() -> this.nextUI = new StartUI(this),
-			Align.BL, 100, 100,
-			textureBank.getTexture("backDefault.png"),
-			textureBank.getTexture("backHover.png"),
-			textureBank.getTexture("backPressed.png")
-		);
+		backButton = new ButtonComponent(() -> this.nextUI = new StartUI(this), Align.BL, 100, 100, textureBank.getTexture("backDefault.png"), textureBank.getTexture("backHover.png"), textureBank.getTexture("backPressed.png"));
 
 		// Create Background Image
-		backgroundImage = new ImageComponent(
-			Align.BL, 0, 0, textureBank.getTexture("Start_BG.png"), 0.0f
-		);
+		backgroundImage = new ImageComponent(Align.BL, 0, 0, textureBank.getTexture("Start_BG.png"), 0.0f);
 
-		connection.getLobbies((lobs) -> {
+		// Create Refresh Button
+		refreshButton = new TextButtonComponent(() -> {
+			connection.getLobbies((lobs) -> {
+				refresh(lobs);
+			}, (err) -> {
+			});
+		}, Align.BL, 200, 80, fontBank.getFont("emulogic.ttf"), 0.5f, "Refresh");
+
+		connection.getLobbies((lobs) ->
+
+		{
 			refresh(lobs);
 		}, (err) -> {
 
@@ -78,97 +78,121 @@ public class LobbyUI extends UI implements InputPipeMulti {
 
 	/**
 	 * Is called when any of the lobbies are selected
+	 * 
 	 * @param lobby
 	 */
-	private void lobbySelect(int lobby) {
-		for (int i=0; i < lobby_buttons.size(); i++) {
+	private void lobbySelect(int lobby)
+	{
+		for (int i = 0; i < lobby_buttons.size(); i++)
+		{
 			lobby_buttons.get(i).setSelected(false);
 		}
 		lobby_buttons.get(lobby).setSelected(true);
 		currentLobby = lobbies.get(lobby);
 	}
 
-	private void refresh(ArrayList<LobbyInfo> lobs) {
+	private void refresh(ArrayList<LobbyInfo> lobs)
+	{
 		lobbies = lobs;
 		lobby_buttons.clear();
 		this.inputHandlers.clear();
-		for (int i = 0; i < lobbies.size(); i++) {
+		for (int i = 0; i < lobbies.size(); i++)
+		{
 			final int l = i;
-			TextButtonComponent lobbyButton = new TextButtonComponent(
-					() -> {lobbySelect(l);}, Align.BL, 300, 300, fontBank.getFont("emulogic.ttf"), 0.5f, lobbies.get(i));
+			TextButtonComponent lobbyButton = new TextButtonComponent(() -> {
+				lobbySelect(l);
+			}, Align.BL, 300, 300, fontBank.getFont("emulogic.ttf"), 0.5f, lobbies.get(i));
 			lobby_buttons.add(lobbyButton);
 			this.inputHandlers.add(lobby_buttons.get(i));
 		}
 		this.inputHandlers.add(joinButton);
 		this.inputHandlers.add(backButton);
+		this.inputHandlers.add(refreshButton);
 	}
 
 	@Override
-	public ArrayList<InputHandler> getHandlers() {
+	public ArrayList<InputHandler> getHandlers()
+	{
 		return this.inputHandlers;
 	}
-	
+
 	@Override
-	public void handleResize(int w, int h) {
+	public void handleResize(int w, int h)
+	{
 		this.windowW = w;
 		this.windowH = h;
 		InputPipeMulti.super.handleResize(w, h);
 	}
-	
+
 	@Override
-	public void handleKey(int key, int scancode, int action, int mods) {
-		
+	public void handleKey(int key, int scancode, int action, int mods)
+	{
+
 		InputPipeMulti.super.handleKey(key, scancode, action, mods);
 		// Allows escape to be pressed to return to previous menu
-		if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS){
-		 	System.out.println("escape pressed");
+		if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+		{
+			System.out.println("escape pressed");
 			this.nextUI = new StartUI(this);
 		}
 	}
-	
+
 	@Override
-	public void update(double dt) {
+	public void update(double dt)
+	{
 		// Run the update method for all of the buttons
 		joinButton.update(dt);
 		backButton.update(dt);
-		for (int i=0; i < lobby_buttons.size(); i++) {
+		refreshButton.update(dt);
+		for (int i = 0; i < lobby_buttons.size(); i++)
+		{
 			lobby_buttons.get(i).update(dt);
 		}
 	}
 
 	@Override
-	public void render(IRenderer r) {
-		//Set locations of the primary menu buttons
-		joinButton.setX((int) (windowW/2.0 - joinButton.getWidth()/2.0));
-		joinButton.setY((int) (windowH/2.0 - joinButton.getHeight()/2.0) + 150);
-		backButton.setX((int) (windowW/2.0 - backButton.getWidth()/2.0));
-		backButton.setY((int) (windowH/2.0 - backButton.getHeight()/2.0));
+	public void render(IRenderer r)
+	{
+		// Set locations of the primary menu buttons
+		joinButton.setX((int) (windowW / 2.0 - joinButton.getWidth() / 2.0));
+		joinButton.setY((int) (windowH / 2.0 - joinButton.getHeight() / 2.0) + 150);
+		backButton.setX((int) (windowW / 2.0 - backButton.getWidth() / 2.0));
+		backButton.setY((int) (windowH / 2.0 - backButton.getHeight() / 2.0));
 
-		//Render these and the background image
+		refreshButton.setY((int) (windowH - refreshButton.getHeight() - 10));
+		refreshButton.setX((int) (windowW - refreshButton.getWidth() - 10));
+
+		// Render these and the background image
 		backgroundImage.render(r);
 		joinButton.render(r);
 		backButton.render(r);
 
+		refreshButton.render(r);
+
 		// Set location of and render each of the lobby buttons
-		for (int i=0; i < lobby_buttons.size(); i++) {
-			lobby_buttons.get(i).setX((int) (windowW/2.0 - lobby_buttons.get(i).getWidth()/2.0));
-			lobby_buttons.get(i).setY((int) (windowH/2.0 - lobby_buttons.get(i).getHeight()/2.0) - 60 - (i+1)*lobby_spacing);
+		for (int i = 0; i < lobby_buttons.size(); i++)
+		{
+			lobby_buttons.get(i).setX((int) (windowW / 2.0 - lobby_buttons.get(i).getWidth() / 2.0));
+			lobby_buttons.get(i).setY((int) (windowH / 2.0 - lobby_buttons.get(i).getHeight() / 2.0) - 60 - (i + 1) * lobby_spacing);
 			lobby_buttons.get(i).render(r);
 		}
 	}
 
 	@Override
-	public UI next() {
+	public UI next()
+	{
 		return nextUI;
 	}
 
 	@Override
-	public String toString() {
+	public String toString()
+	{
 		return "LobbyUI";
 	}
 
 	@Override
-	public void destroy() {
+	public void destroy()
+	{
 		// Nothing to destroy
 	}
 
--- src/game/ui/TextButtonComponent.java
+++ src/game/ui/TextButtonComponent.java
@@ -8,15 +8,18 @@ import game.render.Font;
 import game.render.IRenderer;
 import org.joml.Vector4f;
 
+import com.sun.xml.internal.ws.policy.privateutil.PolicyUtils.Text;
+
 import static org.lwjgl.glfw.GLFW.*;
 
-public class TextButtonComponent extends UIComponent {
+public class TextButtonComponent extends UIComponent
+{
 
 	/** The function that is called when the button is clicked */
 	private Runnable callback;
 
 	private Vector4f box_colour = ColorUtil.BLACK;
-    private Vector4f border_colour = ColorUtil.WHITE;
+	private Vector4f border_colour = ColorUtil.WHITE;
 
 	/** The current mouse x */
 	private float mx;
@@ -28,16 +31,16 @@ public class TextButtonComponent extends UIComponent {
 	/** The current button y */
 	private float y;
 
-	//** The dimensions of the box */
+	// ** The dimensions of the box */
 	private float BOX_W;
 	private float BOX_H;
 	private float border_width;
-	//* The alignment of the box/button */
+	// * The alignment of the box/button */
 	private Align a;
 
-	//* The font used for the text of the button */
+	// * The font used for the text of the button */
 	private Font f;
-	//* The scale of the text */
+	// * The scale of the text */
 	private float scale;
 
 	/** The info of the lobby */
@@ -51,7 +54,10 @@ public class TextButtonComponent extends UIComponent {
 	/** Whether the mouse has been released since the last update */
 	private boolean released = false;
 
-	public TextButtonComponent(Runnable _callback, Align _a, float _x, float _y, Font _f, float scale, LobbyInfo lobbyInfo) {
+	private String text;
+
+	public TextButtonComponent(Runnable _callback, Align _a, float _x, float _y, Font _f, float scale, LobbyInfo lobbyInfo)
+	{
 
 		this.callback = _callback;
 		this.a = _a;
@@ -61,38 +67,62 @@ public class TextButtonComponent extends UIComponent {
 		this.scale = scale;
 		this.lobbyInfo = lobbyInfo;
 
-
 		border_width = 5;
 		BOX_W = 800;
-		BOX_H = 64*scale*2;
+		BOX_H = 64 * scale * 2;
+
+	}
+
+	public TextButtonComponent(Runnable _callback, Align _a, float height, float width, Font _f, float scale, String text)
+	{
+
+		this.callback = _callback;
+		this.a = _a;
+		this.x = 0;
+		this.y = 0;
+		this.f = _f;
+		this.scale = scale;
+		this.lobbyInfo = null;
+
+		this.text = text;
+
+		border_width = 10;
+		BOX_W = height;
+		BOX_H = width;
 
 	}
 
 	@Override
-	public void handleCursorPos(double xpos, double ypos) {
+	public void handleCursorPos(double xpos, double ypos)
+	{
 		this.mx = (float) xpos;
 		this.my = (float) ypos;
 	}
 
 	@Override
-	public void handleMouseButton(int button, int action, int mods) {
-		if (action == GLFW_PRESS && button == GLFW_MOUSE_BUTTON_1) {
+	public void handleMouseButton(int button, int action, int mods)
+	{
+		if (action == GLFW_PRESS && button == GLFW_MOUSE_BUTTON_1)
+		{
 			this.pressed = true;
 		}
 
-		if (action == GLFW_RELEASE && button == GLFW_MOUSE_BUTTON_1) {
+		if (action == GLFW_RELEASE && button == GLFW_MOUSE_BUTTON_1)
+		{
 			this.released = true;
 		}
 	}
 
-	private boolean isMouseOnButton() {
-		return mx >= x && mx < x + BOX_W
-			&& my >= y && my < y + BOX_H;
+	private boolean isMouseOnButton()
+	{
+		return mx >= x && mx < x + BOX_W && my >= y && my < y + BOX_H;
 	}
 
 	@Override
-	public void update(double dt) {
-		if (this.released) {
+	public void update(double dt)
+	{
+		if (this.released)
+		{
 			this.pressed = false;
 			this.released = false;
 			if (isMouseOnButton())
@@ -100,71 +130,95 @@ public class TextButtonComponent extends UIComponent {
 		}
 
 		// Set the colour of the border of the box based on select or hover
-		if (selected) {
-            border_colour = ColorUtil.WHITE;
-		} else if (isMouseOnButton()) {
-            border_colour = ColorUtil.LIGHT_GREY;
-		} else {
-            border_colour = ColorUtil.DARK_GREY;
+		if (selected)
+		{
+			border_colour = ColorUtil.WHITE;
+		} else if (isMouseOnButton())
+		{
+			border_colour = ColorUtil.LIGHT_GREY;
+		} else
+		{
+			border_colour = ColorUtil.DARK_GREY;
 		}
 	}
 
 	@Override
-	public void render(IRenderer r) {
+	public void render(IRenderer r)
+	{
 		// Draw the outer box (also acts as the border)
 		r.drawBox(a, x, y, BOX_W, BOX_H, border_colour);
 		// Draw the inner box
-		r.drawBox(a, x+border_width, y+border_width, BOX_W-2*border_width, BOX_H-2*border_width, box_colour);
+		r.drawBox(a, x + border_width, y + border_width, BOX_W - 2 * border_width, BOX_H - 2 * border_width, box_colour);
 		// Draw the name of the lobby
-		r.drawText(f, lobbyInfo.getLobbyName(), a, false, x+15, y-2, scale);
-		// Draw the current and max number of players
-		// TODO: display correct number of players in the lobby
-		r.drawText(f, lobbyInfo.getPlayerInfo().length+"/"+lobbyInfo.getMaxPlayers()+" Players", a, false, x+460, y-2, scale);
+		if (lobbyInfo != null)
+		{
+			r.drawText(f, lobbyInfo.getLobbyName(), a, false, x + 15, y - 2, scale);
+			// Draw the current and max number of players
+			// TODO: display correct number of players in the lobby
+			r.drawText(f, lobbyInfo.getPlayerInfo().length + "/" + lobbyInfo.getMaxPlayers() + " Players", a, false, x + 460, y - 2, scale);
+		} else
+		{
+			r.drawText(f, text, a, false, x + 4, y, scale);
+		}
 	}
 
 	/**
 	 * Returns whether the button is selected or not
+	 * 
 	 * @return boolean Is the button currently selected?
 	 */
-	public boolean getSelected() {
+	public boolean getSelected()
+	{
 		return selected;
 	}
 
 	/**
 	 * Sets the button to be selected or not selected
-	 * @param b true/false based on whether you want the button to be selected or not
+	 * 
+	 * @param b
+	 *            true/false based on whether you want the button to be selected
+	 *            or not
 	 */
-	public void setSelected(boolean b) {
+	public void setSelected(boolean b)
+	{
 		selected = b;
 	}
 
 	/**
 	 * Sets the y co-ordinate of the button
-	 * @param _x The x co-ordinate
+	 * 
+	 * @param _x
+	 *            The x co-ordinate
 	 */
-	public void setX(float _x) {
+	public void setX(float _x)
+	{
 		this.x = _x;
 	}
 
 	/**
 	 * Sets the y co-ordinate of the button
-	 * @param _y The y co-ordinate
+	 * 
+	 * @param _y
+	 *            The y co-ordinate
 	 */
-	public void setY(float _y) {
+	public void setY(float _y)
+	{
 		this.y = _y;
 	}
 
 	/**
 	 * Returns the width of the button
 	 */
-	public int getWidth() {
+	public int getWidth()
+	{
 		return (int) BOX_W;
 	}
 
 	/**
 	 * Returns the height of the button
 	 */
-	public int getHeight() {
+	public int getHeight()
+	{
 		return (int) BOX_H;
 	}
 
