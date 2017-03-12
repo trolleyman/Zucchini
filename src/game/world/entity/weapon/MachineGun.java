95669e6150da9e49a03b72c968636b7e0f657f03
yxh561 <yxh561@cs.bham.ac.uk>
2017-03-12 19:08:30 +0000

    Some cleanup

--- src/game/world/entity/weapon/MachineGun.java
+++ src/game/world/entity/weapon/MachineGun.java
@@ -46,7 +46,6 @@ public class MachineGun extends Weapon {
 		if (this.reloadSoundID == -1) {
 			System.out.println("Reloading machine gun...");
 			this.reloadSoundID = ua.audio.play("gun_reload[2sec].wav", 0.6f, this.position);
-			System.out.println("Set reload Sound: "+ reloadSoundID);
 		}else{
 			ua.audio.updateSourcePos(this.reloadSoundID, this.position);
 		}
