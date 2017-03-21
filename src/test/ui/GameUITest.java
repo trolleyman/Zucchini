57ba066db21e3298275b421abd5e1d1dc7845f89
Abby Wiggins <abbylaura@Judass-MacBook-Pro.local>
2017-03-21 10:33:07 +0000

    Adding GameUITest

--- src/test/ui/GameUITest.java
+++ src/test/ui/GameUITest.java
@@ -2,12 +2,16 @@ package test.ui;
 
 import static org.junit.Assert.*;
 import org.junit.Test;
+import game.ui.GameUI;
 
 public class GameUITest {
 
+	
+	GameUI gameui = new GameUI(null, null);
+	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testWorld() {
+		assertNotNull(gameui.world);
 	}
 
 }
